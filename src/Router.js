66bb33240686a64c23486a25a782d469c962ffbb
Erik <elekerik@gmail.com>
2017-04-26 12:14:24 +0100

    Added react-native-router-flux

---
+++ src/Router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+
+const RouterComponent = () => {
+  return (
+    <Router sceneStyle={{ paddingTop: 65 }}>
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Scene>
+
+      <Scene>
+        <Scene key="employeelist" component={EmployeeList} title="Employees" />
+      </Scene>
+    </Router>
+  );
+};
+
+export default RouterComponent;
